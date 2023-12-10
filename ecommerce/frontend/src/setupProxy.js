ccbdaf9587bc5ec515989c83424a57f7e2f6b868
Andre Ferreira <aferreira1535@gmail.com>
2023-12-09 22:18:58 -0300

    proxy

--- ecommerce/frontend/src/setupProxy.js
+++ ecommerce/frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     "/api/v1",
     createProxyMiddleware({
-      target: "http://172.23.64.1:4000",
+      target: "http://192.168.237.98:4000",
       changeOrigin: true,
     })
   );
